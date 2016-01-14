01c7a6dbb7cc6c834cea459f7774163ab4851408
Souranil Sen <souranil@gmail.com>
2016-01-14 19:32:08 +0530

    Adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp'),
+  mocha = require('gulp-mocha');
+
+
+
+gulp.task('test', function() {
+  return gulp.src('./test/**/*.test.js', {read: false})
+    .pipe(mocha())
+        .once('error', () => {
+            process.exit(1);
+        })
+        .once('end', () => {
+            process.exit();
+        });
+    
+})
+
+
+gulp.task('default');
